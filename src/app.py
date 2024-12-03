d242307a31536b1be04fa8aba4250277b38d0330
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-12-03 03:25:46 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -14,8 +14,16 @@ def hello_world():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    json_text = jsonify(todos)
+    return json_text
 
 
 if __name__ == '__main__':
